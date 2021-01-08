308bbbbe65c1807a230015d1fc0724ca68932b6e
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2021-01-08 00:15:50 -0500

    Fix compilation

--- loop_closure_transform/src/loop_closure_transform_node.cpp
+++ loop_closure_transform/src/loop_closure_transform_node.cpp
@@ -1,31 +1,36 @@
 #include <ros/ros.h>
-#include <kimera-vio/loopclosure/LoopClosureDetector.h>
+#include <queue>
+#include "kimera-vio/loopclosure/LoopClosureDetector.h"
+#include "loop_closure_transform/StereoMatch.h"
 
 class LCDHandler
 {
 public:
-    LCDMessageHandler(VIO::LoopClosureDetectorParams lcd_params){
-        lcd_ = VIO::LoopClosureDetector(lcd_params, true);
+    LCDHandler(VIO::LoopClosureDetectorParams lcd_params):
+        lcd_(lcd_params, true)
+    {
     };
 
-    void verify_match(const StereoMatch::ConstPtr &msg)
+    void verify_match(const loop_closure_transform::StereoMatch::ConstPtr &msg)
     {
-    
+        //VIO::StereoFrame stereo_frame_;
+        //lcd_.processAndAddFrame(stereo_frame_);
     }
 
     void process_next_match() {
-        /*LoopClosureDetector::geometricVerificationNister(
+
+        /*lcd_.geometricVerificationNister(
         const FrameId& query_id,
         const FrameId& match_id,
         gtsam::Pose3* camCur_T_camRef_mono);*/
+        
 
         // recoverPose
     }
 
 private:
     VIO::LoopClosureDetector lcd_;
-    // Create Queue
-
+    std::queue<std::pair<VIO::FrameId, VIO::FrameId>> match_queue_;
 };
 
 
@@ -35,16 +40,16 @@ int main(int argc, char** argv) {
     ros::init( argc, argv, "loop_closure_transform" );
     ros::NodeHandle nh_;
 
-    // Subscriber
-    ros::Subscriber sub_match = nh_.subscribe("verify_match", 1000, &verify_match);
-
     // Geometric transformation engine
     VIO::LoopClosureDetectorParams lcd_params;
-    
+    LCDHandler handler(lcd_params);
+
+    // Subscriber
+    ros::Subscriber sub_match = nh_.subscribe("verify_match", 1000, &LCDHandler::verify_match, &handler);
 
     // Main loop
     while(ros::ok()){
-        process_next_match();
+        handler.process_next_match();
         ros::spinOnce();
     }
 
