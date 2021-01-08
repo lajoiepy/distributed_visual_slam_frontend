9bb180ae4b0c007022700cf8597732f6a058c56b
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2021-01-07 21:34:57 -0500

    Unstable transform prelim

--- loop_closure_transform/src/loop_closure_transform_node.cpp
+++ loop_closure_transform/src/loop_closure_transform_node.cpp
@@ -1,16 +1,53 @@
 #include <ros/ros.h>
 #include <kimera-vio/loopclosure/LoopClosureDetector.h>
 
+class LCDHandler
+{
+public:
+    LCDMessageHandler(VIO::LoopClosureDetectorParams lcd_params){
+        lcd_ = VIO::LoopClosureDetector(lcd_params, true);
+    };
+
+    void verify_match(const StereoMatch::ConstPtr &msg)
+    {
+    
+    }
+
+    void process_next_match() {
+        /*LoopClosureDetector::geometricVerificationNister(
+        const FrameId& query_id,
+        const FrameId& match_id,
+        gtsam::Pose3* camCur_T_camRef_mono);*/
+
+        // recoverPose
+    }
+
+private:
+    VIO::LoopClosureDetector lcd_;
+    // Create Queue
+
+};
+
+
+
 int main(int argc, char** argv) {
-   // Intialization
-   ros::init( argc, argv, "loop_closure_transform" );
-   ros::NodeHandle nh_;
+    // Initialization
+    ros::init( argc, argv, "loop_closure_transform" );
+    ros::NodeHandle nh_;
+
+    // Subscriber
+    ros::Subscriber sub_match = nh_.subscribe("verify_match", 1000, &verify_match);
+
+    // Geometric transformation engine
+    VIO::LoopClosureDetectorParams lcd_params;
+    
 
     // Main loop
     while(ros::ok()){
+        process_next_match();
         ros::spinOnce();
     }
 
-   // All done
-   return 0;
+    // All done
+    return 0;
 }
