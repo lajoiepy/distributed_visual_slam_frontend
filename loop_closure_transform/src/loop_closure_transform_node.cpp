25f2f9d3c68dbc96477e37603218e34cba0e609e
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2021-01-08 15:21:24 -0500

    Little progress

--- loop_closure_transform/src/loop_closure_transform_node.cpp
+++ loop_closure_transform/src/loop_closure_transform_node.cpp
@@ -11,10 +11,18 @@ public:
     {
     };
 
-    void verify_match(const loop_closure_transform::StereoMatch::ConstPtr &msg)
+    void verify_match_images(const loop_closure_transform::StereoMatch::ConstPtr &msg)
     {
-        //VIO::StereoFrame stereo_frame_;
-        //lcd_.processAndAddFrame(stereo_frame_);
+        CameraParams cam_param_left_0()
+        VIO::StereoFrame stereo_frame_0(    msg->id_0, 
+                                            msg->left_0.header.stamp,
+                                            VIO::readRosImage(msg->left_0),
+                                            CameraParams& cam_param_left,
+                                            cv::Mat& right_image,
+                                            CameraParams& cam_param_right,
+                                            StereoMatchingParams& stereo_matching_params)
+        
+        FrameId = lcd_.processAndAddFrame(stereo_frame_0);
     }
 
     void process_next_match() {
@@ -38,14 +46,15 @@ private:
 int main(int argc, char** argv) {
     // Initialization
     ros::init( argc, argv, "loop_closure_transform" );
-    ros::NodeHandle nh_;
+    ros::NodeHandle nh;
 
     // Geometric transformation engine
     VIO::LoopClosureDetectorParams lcd_params;
     LCDHandler handler(lcd_params);
 
     // Subscriber
-    ros::Subscriber sub_match = nh_.subscribe("verify_match", 1000, &LCDHandler::verify_match, &handler);
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber sub_match = it.subscribe("verify_match_images", 1000, &LCDHandler::verify_match_images, &handler);
 
     // Main loop
     while(ros::ok()){
